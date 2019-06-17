7210dbfaabb2a134e4ce215ec185cd3cbf2ee4e8
Rosellyne <elijahroselyne@gmail.com>
2019-06-17 06:25:09 +0300

    added the system println to ask the user to enter a string

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,13 @@ import java.io.Console;
 
 public class App {
     public static void main(String[] args) {
-        
+        Console myConsole = System.console();
+        System.out.println(Enter the string to be encrypted);
+        String plainText = myConsole.readLine();
+
+//        System.out.println(Enter the shift key);
+//        String shiftkey= myConsole.readLine();
+//        int intKey =Integer.parseInt(shiftkey);
+
     }
 }
