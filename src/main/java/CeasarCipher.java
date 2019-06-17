2cfe29b77fcb182d37c37569fd400c0d9738ab83
Rosellyne <elijahroselyne@gmail.com>
2019-06-17 06:04:12 +0300

    add the private modifier to the string and int key

--- src/main/java/CeasarCipher.java
+++ src/main/java/CeasarCipher.java
@@ -1,6 +1,21 @@
 
 public class CeasarCipher {
+    private String mName;
+    private int mKey;
+    
+    public int getmKey() {
+        return mKey;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+
+    }
     public String Encrypt( String fine,int key) {
+        mName = fine;
+        mKey = key;
         String result = "";
         for (int counter = 0; counter < fine.length(); counter++) {
             char text = fine.charAt(counter);
