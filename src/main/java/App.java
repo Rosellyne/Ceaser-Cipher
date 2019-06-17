e3cc5782fe1b0e84e355928e9a9b2e59ce6e7a0c
Rosellyne <elijahroselyne@gmail.com>
2019-06-17 10:03:46 +0300

    add the title and description to the README.md

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,38 +3,41 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
+        boolean programRunning =true;
         Console myConsole = System.console();
-        System.out.println("Welcome to  Ceasar Cipher.What do you like to do? \n1.Encrypt \n2.Decrypt \n3.Quit");
-        try {
-            String navigateChoice = myConsole.readLine();
-            if (navigateChoice.equals("1")) {
-                System.out.println("Enter a text to be encrypted");
-                String plainText = myConsole.readLine();
-                System.out.println("Enter the shift key to shift your text");
-                String shiftkey = myConsole.readLine();
-                int intKey = Integer.parseInt(shiftkey);
-                Encrypt encrypt = new Encrypt(plainText, intKey);
-                System.out.println(String.format("Your encrypted test is %s.", encrypt.isEncrypt()));
-                System.out.println("--------------------------------------------------------");
-            } else if (navigateChoice.equals("2")) {
-                System.out.println("Enter a text to be decrypted");
-                String plainText = myConsole.readLine();
-                System.out.println("Enter the shift key to shift your text");
-                String shiftkey = myConsole.readLine();
-                int intKey = Integer.parseInt(shiftkey);
-                Decrypt decrypt = new Decrypt(plainText, intKey);
-                System.out.println(String.format("Your decrypted test is %s.", decrypt.isDecrypt()));
-                System.out.println("--------------------------------------------------------");
-            } else if (navigateChoice.equals("3")) {
-                System.out.println("Goodbye");
-                System.out.println("--------------------------------------------------------");
-            } else {
-                System.out.println("you have entered the invalid input");
+        while(programRunning) {
+            System.out.println("Welcome to  Ceasar Cipher.What do you like to do? \n1.Encrypt \n2.Decrypt \n3.Quit");
+            try {
+                String navigateChoice = myConsole.readLine();
+                if (navigateChoice.equals("1")) {
+                    System.out.println("Enter a text to be encrypted");
+                    String plainText = myConsole.readLine();
+                    System.out.println("Enter the shift key to shift your text");
+                    String shiftkey = myConsole.readLine();
+                    int intKey = Integer.parseInt(shiftkey);
+                    Encrypt encrypt = new Encrypt(plainText, intKey);
+                    System.out.println(String.format("Your encrypted test is %s.", encrypt.isEncrypt()));
+                    System.out.println("--------------------------------------------------------");
+                } else if (navigateChoice.equals("2")) {
+                    System.out.println("Enter a text to be decrypted");
+                    String plainText = myConsole.readLine();
+                    System.out.println("Enter the shift key to shift your text");
+                    String shiftkey = myConsole.readLine();
+                    int intKey = Integer.parseInt(shiftkey);
+                    Decrypt decrypt = new Decrypt(plainText, intKey);
+                    System.out.println(String.format("Your decrypted test is %s.", decrypt.isDecrypt()));
+                    System.out.println("--------------------------------------------------------");
+                } else if (navigateChoice.equals("3")) {
+                    System.out.println("Goodbye");
+                    programRunning = false;
+                    System.out.println("--------------------------------------------------------");
+                } else {
+                    System.out.println("you have entered the invalid input");
+                }
+            } catch (NullPointerException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
     }
     }
 
---
+++ src/main/java/README.md
@@ -0,0 +1,9 @@
+## CaesarCipher
+
++ By Roselyne Moraa
+
+## Description
+
+This is an application that enables a user to enter a word and encrypt it or decypt it by a certain key value.
+
+## 
